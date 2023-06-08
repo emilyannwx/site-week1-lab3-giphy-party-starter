1501abcb04e3e591469d81183bcc949d69ecfc25
emilyannwx <121910172+emilyannwx@users.noreply.github.com>
2023-06-08 10:51:45 -0400

    added html elements (form, text input, buttons

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Global Constants
-const apiKey = "MY_API_KEY"
+const apiKey = "6893req95Aj3boJhu1mllYB2TQ7Lz4vD"
+const GIPHY_API_BASE_URL = `http://api.giphy.com/v1/gifs/search`
+const limit = 15
+const rating = 'g'
+
+let pageNum = 0
+let offset = 0
+
+const search_form = document.getElementById("search-form")
+const search_input = document.getElementById("search-input")
+const submit_btn = document.getElementById("search-button")
+const dis_results = document.getElementById("display")
+const more_btn = document.getElementById("load-more")
 
 /**
+ * 
  * Update the DOM to display results from the Giphy API query.
+ * 
  *
  * @param {Object} results - An array of results containing each item
  *                           returned by the response from the Giphy API.
@@ -10,6 +24,13 @@ const apiKey = "MY_API_KEY"
  */
 function displayResults(results) {
   // YOUR CODE HERE
+  display.innerHTML = ''
+  results.data.forEach((element) => {
+    let gif = document.createElement('img');
+    gif.src = element.images.original.url
+    display.innerHTML +=`<img src="${gif.src}" alt="image">`
+  })
+
 }
 
 /**
@@ -21,6 +42,10 @@ function displayResults(results) {
  */
 async function getGiphyApiResults(searchTerm) {
   // YOUR CODE HERE
+  const res = await fetch (`${GIPHY_API_BASE_URL}?q=${searchTerm}&limit=${limit}&offset=${offset}&api_key=${apiKey}`)
+  const data = await res.json()
+  console.log (data)
+  displayResults(data)
 }
 
 /**
@@ -30,7 +55,10 @@ async function getGiphyApiResults(searchTerm) {
  *
  */
 async function handleFormSubmit(event) {
-  // YOUR CODE HERE
+  event.preventDefault() // stops from from refreshing
+  console.log(search_input.value)
+  getGiphyApiResults(search_input.value)
+  more_btn?.classList?.remove?.("hidden")
 }
 
 // searchForm.addEventListener("submit", handleFormSubmit)
@@ -44,9 +72,16 @@ async function handleFormSubmit(event) {
  */
 async function handleShowMore(event) {
   // YOUR CODE HERE
+  offset += 15
+  let res = document.getElementById('search-input');
+  let data = await getGiphyApiResults(res.value)
+  displayResults(data)
 }
 
 window.onload = function () {
   // YOUR CODE HERE
   // Add any event handlers here
+ 
+ submit_btn.addEventListener("click", handleFormSubmit)
+ more_btn.addEventListerner("click", handleShowMore)
 }
